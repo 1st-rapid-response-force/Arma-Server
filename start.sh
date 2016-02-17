c4d18f5d51783ec22c5c91880f17b168396fb06c
Alexander Christie <alex@briefly.io>
2016-02-17 19:49:21 +0000

    I always forget task force radio
    Signed-off-by: Alexander Christie <alex@briefly.io>

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./arma3server -config="server.cfg" -maxMem=4094 -exThread=7 -enableHT -port=2302 -autoInit -name="1st_rrf" -profiles="/home/server/arma/profiles" -mod="modpack/@cba_a3;modpack/@ares;modpack/@ace;modpack/@rhs_afrf;modpack/@rhs_usaf;modpack/@1rrf_maps;modpack/@1rrf_content;modpack/@1rrf_utility"
+./arma3server -config="server.cfg" -maxMem=4094 -exThread=7 -enableHT -port=2302 -autoInit -name="1st_rrf" -profiles="/home/server/arma/profiles" -mod="modpack/@cba_a3;modpack/@task_force_radio;modpack/@ares;modpack/@ace;modpack/@rhs_afrf;modpack/@rhs_usaf;modpack/@1rrf_maps;modpack/@1rrf_content;modpack/@1rrf_utility"
